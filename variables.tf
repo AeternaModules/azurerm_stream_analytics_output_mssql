892906d98cdbc7dba31d96c66a37959a355f11f4
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:33:02 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "stream_analytics_output_mssqls" {
+  description = <<EOT
+Map of stream_analytics_output_mssqls, attributes below
+Required:
+    - database
+    - name
+    - resource_group_name
+    - server
+    - stream_analytics_job_name
+    - table
+Optional:
+    - authentication_mode
+    - max_batch_count
+    - max_writer_count
+    - password
+    - user
+EOT
+
+  type = map(object({
+    database                  = string
+    name                      = string
+    resource_group_name       = string
+    server                    = string
+    stream_analytics_job_name = string
+    table                     = string
+    authentication_mode       = optional(string, "ConnectionString")
+    max_batch_count           = optional(number, 10000)
+    max_writer_count          = optional(number, 1)
+    password                  = optional(string)
+    user                      = optional(string)
+  }))
+}
+
