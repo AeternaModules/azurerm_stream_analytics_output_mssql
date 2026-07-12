fb02368ae8aeb2924a60cfdc2641fe0adfef556c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:57 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,51 +1,46 @@
-output "stream_analytics_output_mssqls" {
-  description = "All stream_analytics_output_mssql resources"
-  value       = azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls
-  sensitive   = true
-}
 output "stream_analytics_output_mssqls_authentication_mode" {
-  description = "List of authentication_mode values across all stream_analytics_output_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : v.authentication_mode]
+  description = "Map of authentication_mode values across all stream_analytics_output_mssqls, keyed the same as var.stream_analytics_output_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : k => v.authentication_mode }
 }
 output "stream_analytics_output_mssqls_database" {
-  description = "List of database values across all stream_analytics_output_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : v.database]
+  description = "Map of database values across all stream_analytics_output_mssqls, keyed the same as var.stream_analytics_output_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : k => v.database }
 }
 output "stream_analytics_output_mssqls_max_batch_count" {
-  description = "List of max_batch_count values across all stream_analytics_output_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : v.max_batch_count]
+  description = "Map of max_batch_count values across all stream_analytics_output_mssqls, keyed the same as var.stream_analytics_output_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : k => v.max_batch_count }
 }
 output "stream_analytics_output_mssqls_max_writer_count" {
-  description = "List of max_writer_count values across all stream_analytics_output_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : v.max_writer_count]
+  description = "Map of max_writer_count values across all stream_analytics_output_mssqls, keyed the same as var.stream_analytics_output_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : k => v.max_writer_count }
 }
 output "stream_analytics_output_mssqls_name" {
-  description = "List of name values across all stream_analytics_output_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : v.name]
+  description = "Map of name values across all stream_analytics_output_mssqls, keyed the same as var.stream_analytics_output_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : k => v.name }
 }
 output "stream_analytics_output_mssqls_password" {
-  description = "List of password values across all stream_analytics_output_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : v.password]
+  description = "Map of password values across all stream_analytics_output_mssqls, keyed the same as var.stream_analytics_output_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : k => v.password }
   sensitive   = true
 }
 output "stream_analytics_output_mssqls_resource_group_name" {
-  description = "List of resource_group_name values across all stream_analytics_output_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : v.resource_group_name]
+  description = "Map of resource_group_name values across all stream_analytics_output_mssqls, keyed the same as var.stream_analytics_output_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : k => v.resource_group_name }
 }
 output "stream_analytics_output_mssqls_server" {
-  description = "List of server values across all stream_analytics_output_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : v.server]
+  description = "Map of server values across all stream_analytics_output_mssqls, keyed the same as var.stream_analytics_output_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : k => v.server }
 }
 output "stream_analytics_output_mssqls_stream_analytics_job_name" {
-  description = "List of stream_analytics_job_name values across all stream_analytics_output_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : v.stream_analytics_job_name]
+  description = "Map of stream_analytics_job_name values across all stream_analytics_output_mssqls, keyed the same as var.stream_analytics_output_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : k => v.stream_analytics_job_name }
 }
 output "stream_analytics_output_mssqls_table" {
-  description = "List of table values across all stream_analytics_output_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : v.table]
+  description = "Map of table values across all stream_analytics_output_mssqls, keyed the same as var.stream_analytics_output_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : k => v.table }
 }
 output "stream_analytics_output_mssqls_user" {
-  description = "List of user values across all stream_analytics_output_mssqls"
-  value       = [for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : v.user]
+  description = "Map of user values across all stream_analytics_output_mssqls, keyed the same as var.stream_analytics_output_mssqls"
+  value       = { for k, v in azurerm_stream_analytics_output_mssql.stream_analytics_output_mssqls : k => v.user }
 }
 
